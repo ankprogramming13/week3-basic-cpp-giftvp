493706602db958fa669b9c6e92d56578b61e4460
Unknown <viratagift@gmail.com>
2017-11-24 14:43:38 +0700

    ข้อที่1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "miss."<<endl;
+  cout << "virata."<<endl;
+  cout << "pumjean."<<endl;
+  cout << "gift."<<endl;
+  cout << "female."<<endl;
+  cout << "24/02/2542."<<endl;
+  cout << "read book."<<endl;
+  cout << "single."<<endl;
   return 0;
 }
